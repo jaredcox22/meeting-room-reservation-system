43d3df7b1efd4fa7b696112b3e564ea43b297a07
Jared Cox <jcox@ircuwd.com>
2026-03-16 11:54:27 -0400

    Update CORS configuration to allow multiple frontend origins for improved flexibility
    - Modified the 'allowed_origins' setting in the CORS configuration to include both the environment variable for the frontend URL and a hardcoded localhost URL, enhancing development and testing capabilities.

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://127.0.0.1:3000')],
+    'allowed_origins' => array_filter([
+        env('FRONTEND_URL', 'http://127.0.0.1:3000'),
+        'http://localhost:3000',
+    ]),
 
     'allowed_origins_patterns' => [],
 
